82be2fc46e73a938e24c3636ded4a198cfbcbd98
Abdul Razak <razak8469@gmail.com>
2021-07-21 12:37:03 -0700

    store image and video files in cloud storage

--- training-data-analyst/courses/developingapps/python/cloudstorage/start/quiz/gcp/storage.py
+++ training-data-analyst/courses/developingapps/python/cloudstorage/start/quiz/gcp/storage.py
@@ -13,63 +13,26 @@
 # limitations under the License.
 
 import os
+from google.cloud import storage
+
 project_id = os.getenv('GCLOUD_PROJECT')
 
-# TODO: Get the Bucket name from the GCLOUD_BUCKET environment variable
+bucket_name = os.getenv('GOOGLE_BUCKET')
 
-
-
-# END TODO
-
-# TODO: Import the storage module
-
-
-
-# END TODO
-
-# TODO: Create a client for Cloud Storage
-
-
-
-# END TODO
-
-# TODO: Use the client to get the Cloud Storage bucket
-
-
-
-# END TODO
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(bucket_name)
 
 """
 Uploads a file to a given Cloud Storage bucket and returns the public url
 to the new object.
 """
+
+
 def upload_file(image_file, public):
-
-    pass
-
-    # TODO: Use the bucket to get a blob object
-
+    blob = bucket.blob(image_file.filename)
+    blob.upload_file_string(image_file.read(), content_type=image_file.content_type)
     
+    if public:
+        blob.make_public()
 
-    # END TODO
-
-    # TODO: Use the blob to upload the file
-
-    
-
-
-    # END TODO
-
-    # TODO: Make the object public
-
-    
-
-
-    # END TODO
-
-
-    # TODO: Modify to return the blob's Public URL
-
-    
-
-    # END TODO
+    return blob.public_url
--- training-data-analyst/courses/developingapps/python/cloudstorage/start/quiz/webapp/questions.py
+++ training-data-analyst/courses/developingapps/python/cloudstorage/start/quiz/webapp/questions.py
@@ -11,11 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: Import the storage module
-
-from quiz.gcp import datastore
-
-# END TODO
+from quiz.gcp import datastore, storage
 
 """
 uploads file into google cloud storage
@@ -26,19 +22,10 @@ def upload_file(image_file, public):
     if not image_file:
         return None
 
-    # TODO: Use the storage client to Upload the file
-    # The second argument is a boolean
+    public_url = storage.upload_file(image_file, public)
 
-    
+    return public_url
 
-    # END TODO
-
-    # TODO: Return the public URL
-    # for the object
-
-    return u''
-
-    # END TODO
 
 """
 uploads file into google cloud storage
@@ -47,14 +34,10 @@ uploads file into google cloud storage
 """
 def save_question(data, image_file):
 
-    # TODO: If there is an image file, then upload it
-    # And assign the result to a new Datastore property imageUrl
-    # If there isn't, assign an empty string
-    
-    
-    
-
-    # END TODO
+    if image_file:
+        data['image_url'] = str(upload_file(image_file, True))
+    else:
+        data['image_url'] = u''
 
     data['correctAnswer'] = int(data['correctAnswer'])
     datastore.save_question(data)
